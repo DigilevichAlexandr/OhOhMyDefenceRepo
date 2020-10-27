455bb17c032d4e64619b7ffa4faed950b3f3c7c3
alexandr digilevich <digilevich.alexandr@gmail.com>
2020-10-27 12:43:26 +0300

    init

---
+++ OhOhMyDefence/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace OhOhMyDefence
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 100F);
+            this.label1.Location = new System.Drawing.Point(11, 213);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(754, 153);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Am F C Em";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(754, 45);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.textBox2.Location = new System.Drawing.Point(12, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(754, 45);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.textBox3.Location = new System.Drawing.Point(12, 114);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(754, 45);
+            this.textBox3.TabIndex = 1;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.textBox4.Location = new System.Drawing.Point(12, 165);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(754, 45);
+            this.textBox4.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 8000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 367);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "OhOhMydefence";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ OhOhMyDefence/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OhOhMyDefence
+{
+    public partial class Form1 : Form
+    {
+        List<string> part1, part2, part3, part4;
+
+        Random random;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            part1 = new List<string>();
+            part2 = new List<string>();
+            part3 = new List<string>();
+            part4 = new List<string>();
+
+            part1.Add("солнечный");
+            part1.Add("траурный");
+            part1.Add("плюшевый");
+            part1.Add("бешеный");
+            part1.Add("памятный");
+            part1.Add("трепетный");
+            part1.Add("базовый");
+            part1.Add("скошенный");
+            part1.Add("преданный");
+            part1.Add("ласковый");
+            part1.Add("пойманный");
+            part1.Add("радужный");
+            part1.Add("огненный");
+            part1.Add("радостный");
+            part1.Add("тензорный");
+            part1.Add("шёлковый");
+            part1.Add("пепельный");
+            part1.Add("ламповый");
+            part1.Add("жаренный");
+            part1.Add("загнанный");
+
+            part2.Add("зайчик");
+            part2.Add("Верник");
+            part2.Add("глобус");
+            part2.Add("ветер");
+            part2.Add("щавель");
+            part2.Add("пёсик");
+            part2.Add("копчик");
+            part2.Add("ландыш");
+            part2.Add("стольник");
+            part2.Add("мальчик");
+            part2.Add("дольщик");
+            part2.Add("Игорь");
+            part2.Add("невод");
+            part2.Add("егерь");
+            part2.Add("пончик");
+            part2.Add("лобстер");
+            part2.Add("жемчуг");
+            part2.Add("кольщик");
+            part2.Add("йогурт");
+            part2.Add("овод");
+
+            part3.Add("стеклянного");
+            part3.Add("ванильного");
+            part3.Add("резонного");
+            part3.Add("широкого");
+            part3.Add("дешёвого");
+            part3.Add("горбатаго");
+            part3.Add("собачьего");
+            part3.Add("исконного");
+            part3.Add("волшебного");
+            part3.Add("картонного");
+            part3.Add("лохматого");
+            part3.Add("арбузного");
+            part3.Add("огромного");
+            part3.Add("запойного");
+            part3.Add("великого");
+            part3.Add("бараньего");
+            part3.Add("вандального");
+            part3.Add("едрёного");
+            part3.Add("парадного");
+            part3.Add("укромного");
+
+            part4.Add("глаза");
+            part4.Add("плова");
+            part4.Add("Пельша");
+            part4.Add("мира");
+            part4.Add("деда");
+            part4.Add("жира");
+            part4.Add("мема");
+            part4.Add("ада");
+            part4.Add("бура");
+            part4.Add("жала");
+            part4.Add("нёба");
+            part4.Add("гунна");
+            part4.Add("хлама");
+            part4.Add("шума");
+            part4.Add("воза");
+            part4.Add("сала");
+            part4.Add("фена");
+            part4.Add("зала");
+            part4.Add("рака");
+            part4.Add("смака");
+            random = new Random();
+
+            textBox1.Text = part1[random.Next() % part1.Count()];
+            textBox2.Text = part2[random.Next() % part1.Count()];
+            textBox3.Text = part3[random.Next() % part1.Count()];
+            textBox4.Text = part4[random.Next() % part1.Count()];
+
+            timer1.Start();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            textBox1.Text = part1[random.Next() % part1.Count() * 2];
+            textBox2.Text = part2[random.Next() % part1.Count() * 2];
+            textBox3.Text = part3[random.Next() % part1.Count() * 2];
+            textBox4.Text = part4[random.Next() % part1.Count() * 2];
+        }
+    }
+}
